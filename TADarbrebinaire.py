88f06cee436b1bf8bbc4412d45d5e248605f4923
AdeuxX <116004438+AdeuxX@users.noreply.github.com>
2025-11-05 14:30:52 +0100

    Add files via upload

---
+++ TADarbrebinaire.py
@@ -0,0 +1,27 @@
+class ArbreBinaire :
+    def __init__(self, item, next_left=None, next_right=None):
+        self.item = item
+        self.right = next_right
+        self.left = next_left
+
+def insert(arbre, element):
+    if arbre == None:
+        return element
+    if element < arbre.key:
+        return ArbreBinaire(arbre.item, insert(arbre.next_left,element), arbre.next_right)
+    elif element > arbre.key:
+        return ArbreBinaire(arbre.item, arbre.next_left ,insert(arbre.next_right,element))
+    else:
+        return Exception("element égaux")
+    
+def recherche(arbre, element_recherche):
+    if arbre.key == element_recherche:
+        return arbre.item
+    if element_recherche < arbre.key : 
+        recherche(arbre.next_left,element_recherche)
+    elif element_recherche> arbre.key:
+        recherche(arbre.next_right,element_recherche)
+
+def suppr():
+    ...
+    
